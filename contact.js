54c5073844e5247dff0c3304c2ba83ec3590dcd1
vqsto <romain.croizat@hotmail.fr>
2015-04-15 10:39:58 +0200

    jéféleconect

--- contact.js
+++ contact.js
@@ -16,15 +16,17 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+
+
+      // At the bottom of the client code
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
-
-  // At the bottom of the client code
-Accounts.ui.config({
-  passwordSignupFields: "USERNAME_ONLY"
-});
 }
